466599c95d0f439879c5ad90ea81f161ba462cf1
shk33 <m.coronel.seg@gmail.com>
2014-12-26 23:44:25 -0600

    Add Basic hero class

---
+++ spec/template_method_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+require_relative '../lib/template_method'
+
+describe "Template method Pattern" do
+  let(:hero) { Hero.new }
+
+  it "has default damage rating of 10" do
+    expect(hero.damage).to eq(10)  
+  end
+
+  it "can attack" do
+    expect(hero.attack).to eq("Attacked dealing 10 damage")  
+  end
+
+end
