44dc5f3fe479bca8b5901dea05e08472923ee86e
shk33 <m.coronel.seg@gmail.com>
2014-12-28 17:02:58 -0600

    Add external iterator

--- lib/iterator_pattern.rb
+++ lib/iterator_pattern.rb
@@ -19,3 +19,22 @@ class Item
   end
 
 end
+
+class InventoryIterator
+  
+  def initialize inventory
+    @items = inventory.items
+    @index = 0
+  end
+
+  def has_next?
+    @index < @items.size
+  end
+
+  def next
+    value = @items[@index]
+    @index+=1
+    value
+  end
+  
+end
